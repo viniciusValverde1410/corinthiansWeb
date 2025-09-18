1c3d6468e3dc8e21e8431f69e5299e5074759091
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-09-18 11:30:01 -0300

    feat: create hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,37 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <section className={styles.heroSection}>
+        <div className={styles["blur-layer-1"]}></div>
+        <div className={styles["blur-layer-2"]}></div>
+        <div className={styles.containerHero}>
+          <h1 className={styles.heroTitle}>
+            Sport Club{" "}
+            <span className={styles.heroTitleHighlight}>Corinthians</span>{" "}
+            Paulista
+          </h1>
+          <p className={styles.heroSubtitle}>
+            Mais que um clube, uma paixão. Tradição, garra e o maior time do
+            Brasil.
+          </p>
+          <div className={styles.heroButtons}>
+            <a href="/historia" className={styles.heroButton}>
+              📖 História
+            </a>
+            <a
+              href="/titulos"
+              className={`${styles.heroButton} ${styles.heroButtonWhite}`}
+            >
+              🏆 Títulos
+            </a>
+            <a href="/jogadores" className={styles.heroButton}>
+              👥 Jogadores
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
