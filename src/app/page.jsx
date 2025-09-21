be5fbc89d38a1d0fc094aa8fef7f0d4bed08cf35
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-09-20 21:46:18 -0300

    feat: create banner section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,6 @@
 import styles from "./page.module.css";
+import { Carousel } from "antd";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,7 +15,8 @@ export default function Home() {
             Paulista
           </h1>
           <p className={styles.heroSubtitle}>
-            Mais de 114 anos de muita história e tradição do time do povo em um só lugar!
+            Mais de 114 anos de muita história e tradição do time do povo em um
+            só lugar!
           </p>
           <div className={styles.heroButtons}>
             <a href="/historia" className={styles.heroButton}>
@@ -32,10 +35,47 @@ export default function Home() {
         </div>
       </section>
 
+      <section className={styles.atrativeSection}>
+        <div className={styles.bannerContainer}>
+          <Image className={styles.bannerImage}
+            src="/images/banner.png"
+            alt="Banner"
+            layout="fill"
+            objectFit="cover"
+          />
+
+          <h2 className={styles.bannerText}>TUDO DO ULTIMO BRASILEIRO A CONQUISTAR O MUNDO!</h2>
+        </div>
+      </section>
+
       <section className={styles.navigationSection}>
         <div className={styles.navigationSectionTitle}>
-          <h2>Conheça o Maior Clube do Brasil</h2>
+          <h2>Conheça mais do time do povo</h2>
           <p>Descubra tudo sobre o Timão</p>
+        </div>
+
+        <div className={styles.navigationSectionContent}>
+          <div className={styles.navigationCarousel}>
+            <Carousel arrows infinite={false} className={styles.carousel}>
+              <div>
+                <div className={styles.contentCarousel}>
+                  <Image />
+                <h2>Conheça a casa da fiel!</h2>
+
+
+                </div>
+              </div>
+              <div>
+                <h3 className={styles.contentCarousel}>2</h3>
+              </div>
+              <div>
+                <h3 className={styles.contentCarousel}>3</h3>
+              </div>
+              <div>
+                <h3 className={styles.contentCarousel}>4</h3>
+              </div>
+            </Carousel>
+          </div>
         </div>
       </section>
     </div>
