34fc58dd3d94bbc02e3886186faa34300b4aa704
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-09-29 22:32:09 -0300

    fix: correct socials icons footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,18 +6,18 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.socials}>
-                {/* <Link href="https://www.facebook.com/corinthians" target="_blank">
-                    <Image src="/images/facebook.png" alt="Facebook" width={32} height={32} />
+                <Link href="https://www.tiktok.com/corinthians" target="_blank">
+                    <Image src="/images/tiktok.png" alt="TikTok " width={40} height={40} />
                 </Link>
                 <Link href="https://www.twitter.com/corinthians" target="_blank">
-                    <Image src="/images/twitter.png" alt="Twitter" width={32} height={32} />
+                    <Image src="/images/twitter.png" alt="Twitter" width={40} height={40} />
                 </Link>
                 <Link href="https://www.instagram.com/corinthians" target="_blank">
-                    <Image src="/images/instagram.png" alt="Instagram" width={32} height={32} />
+                    <Image src="/images/instagram.png" alt="Instagram" width={40} height={40} />
                 </Link>
                 <Link href="https://www.youtube.com/corinthians" target="_blank">
-                    <Image src="/images/youtube.png" alt="YouTube" width={32} height={32} />
-                </Link> */}
+                    <Image src="/images/youtube.png" alt="YouTube" width={40} height={40} />
+                </Link>
             </div>
             <p>© 2025 Corinthians. Todos os direitos reservados.</p>
             <p>Desenvolvido por <Link href="/sobre" target="_blank">Vinicius Valverde</Link></p>
