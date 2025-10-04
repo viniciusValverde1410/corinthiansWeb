42b6e896693fdb38a6a6ddf37352dcfe096e805c
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-10-04 19:40:04 -0300

    fix: correct uniforme

--- src/app/clube/page.jsx
+++ src/app/clube/page.jsx
@@ -153,19 +153,25 @@ export default function Clube() {
               </p>
               <div className={styles.buyButtonsContainer}>
                 <Link
-                  href="/loja/uniforme-titular"
+                  href="https://www.nike.com.br/camisa-nike-corinthians-i-2025-26-jogador-masculina-059012.html?cor=51"
                   className={styles.buyButton}
                 >
                   COMPRAR UNIFORME TITULAR
                 </Link>
                 <Link
-                  href="/loja/uniforme-reserva"
+                  href="https://www.nike.com.br/camisa-nike-corinthians-ii-2025-26-jogador-masculina-058830.html?cor=ID"
                   className={styles.buyButton}
                 >
                   COMPRAR UNIFORME RESERVA
                 </Link>
                 <Link
-                  href="/loja/uniforme-goleiro"
+                  href="https://www.nike.com.br/camisa-corinthians-nike-total-90-iii-2025-26-jogador-masculina-059619.html?cor=ID"
+                  className={styles.buyButton}
+                >
+                  COMPRAR TERCEIRO UNIFORME
+                </Link>
+                <Link
+                  href="https://www.nike.com.br/camisa-de-goleiro-nike-corinthians-2025-26-torcedor-pro-masculina-058813.html?cor=ID&utm_source=google&utm_medium=cpc&utm_campaign=Google_PLA_Vestu%C3%A1rio_Geral&gad_source=1&gad_campaignid=22900875082&gbraid=0AAAAADob9lODEKSp_XYfwty735XYL7QSK&gclid=CjwKCAjwi4PHBhA-EiwAnjTHudKeAg0sd_b5azclWEdYIwuTGx0FDzGk-uUSsoWzpT_Uq7WZl7-e6BoCkkAQAvD_BwE&utm_referrer=https://www.google.com/"
                   className={styles.buyButton}
                 >
                   COMPRAR UNIFORME GOLEIRO
